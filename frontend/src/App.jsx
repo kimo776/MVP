203ad866baf9f13103d7bcefb61b4f4acb5d0428
kimo776 <kimo77tanki@gmail.com>
2026-05-01 09:40:22 +0300

    Import React alongside hooks in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { texts } from './texts';
 import {
   login, getMyProductOrders, createProductOrder, getMyShipments, getMyDeliveryTasks, updateDeliveryTaskStatus,
